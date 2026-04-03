3fe814abb0b81dbc98bdaa7af2489a810dfe7764
sebastian ceballos tapia <sceballost@gmail.com>
2026-04-03 11:30:13 -0300

    Fix template folder path to resolve 500 error

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 import os
 
 def create_app():
-    app = Flask(__name__)
+    # Specify template folder relative to project root
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    app = Flask(__name__, template_folder=template_dir)
+    
     app.config['UPLOAD_FOLDER'] = 'data'
     app.config['MODEL_FOLDER'] = 'models'
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
